dddf6956aeef0b7e4f18dfac4692880241c9b5b1
jrphan <73537516+jrphan@users.noreply.github.com>
2021-06-08 21:56:08 +0700

    firt commit

---
+++ dest/main.js
@@ -0,0 +1,8 @@
+// let menu = document.querySelector('.menus .menu');
+// let scrollY = window.pageYOffset;
+
+// console.log(scrollY);
+
+// if (scrollY > 70) {
+//     menu.classList.add('active');
+// }
